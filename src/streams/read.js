16049d92a095a37d53be9e76ca26750fc260ef8e
Alexander Pashkou <ale.pashkou@gmail.com>
2022-06-03 20:30:20 +0300

    feat: add solution for write and read stream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,17 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 export const read = async () => {
-    // Write your code here 
+  let data = [];
+  const readStream = fs.createReadStream(
+    path.join(__dirname, 'files/fileToRead.txt')
+  );
+  readStream.on('data', (chunk) => {
+    data.push(chunk.toString());
+  });
+  readStream.on('end', () => process.stdout.write(data.join('')));
 };
+read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,18 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 export const write = async () => {
-    // Write your code here 
+  const wStream = fs.createWriteStream(
+    path.join(__dirname, 'files/fileToWrite.txt')
+  );
+  console.log('Enter the information for the write.');
+  process.stdin.on('data', (value) => {
+    wStream.write(value);
+    console.log('Writed!');
+    process.exit();
+  });
 };
+write();
