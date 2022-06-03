4568c50e312cd14fc43e1051b3420f5eddb8f93f
Alexander Pashkou <ale.pashkou@gmail.com>
2022-06-03 21:07:03 +0300

    feat: add solution for zip

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,15 @@
+import fs from 'fs';
+import zlib from 'zlib';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 export const decompress = async () => {
-    // Write your code here 
+  const input = fs.createReadStream(path.join(__dirname, './files/archive.gz'));
+  const output = fs.createWriteStream(
+    path.join(__dirname, './files/fileToCompress.txt')
+  );
+  input.pipe(zlib.createUnzip()).pipe(output);
 };
+decompress();
