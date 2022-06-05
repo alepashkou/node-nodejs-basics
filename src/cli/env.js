0a662721e4d165d991d89e7648cc2f1abef49d8c
Alexander Pashkou <ale.pashkou@gmail.com>
2022-06-05 20:35:57 +0300

    fix: env script

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,10 +1,13 @@
 export const parseEnv = () => {
   const env = process.env;
   const keys = Object.keys(env);
+  const array = [];
   keys.forEach((el) => {
     if (el.substring(0, 4) === 'RSS_') {
-      console.log(el + '=' + env[el]);
+      let item = el + '=' + env[el];
+      array.push(item.trim());
     }
   });
+  console.log(array.join('; '));
 };
 parseEnv();
