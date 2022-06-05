f7e0121577c461e69ba57ef400b6d6b39528a8b7
Alexander Pashkou <ale.pashkou@gmail.com>
2022-06-05 10:48:56 +0300

    feat: add solution for transform stream

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,12 @@
+import { Transform } from 'stream';
+const transformStream = new Transform({
+  transform(chunk, encoding, callback) {
+    callback(null, chunk.toString().toUpperCase());
+  },
+});
+
 export const transform = async () => {
-    // Write your code here 
+  console.log('Enter data');
+  process.stdin.pipe(transformStream).pipe(process.stdout);
 };
+transform();
